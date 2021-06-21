10c0742c0b1cc04beae5704a94e04309468092c8
Kartik S <278928+Kartiku@users.noreply.github.com>
2021-06-21 19:54:16 -0400

    set AR and RANLIB
    as shown  in https://stackoverflow.com/a/66760990
    it is required for openssl-1.1.1 but not for openssl-1.1.1k

--- build-openssl.sh
+++ build-openssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$(echo -n $ANDROID_NDK/toolchains/llvm/prebuilt/*/bin):$PATH
 ./Configure android-arm64 no-asm
-make clean build_libs CC=x86_64-linux-android21-clang CFLAG="-fPIC -DOPENSSL_THREADS -D_REENTRANT -DDSO_DLFCN -DHAVE_DLFCN_H -D__ANDROID_API__=21 -Os -fomit-frame-pointer -Wall -I$ANDROID_NDK_ROOT/sysroot/usr/include -I$ANDROID_NDK_ROOT/sysroot/usr/include/i686-linux-android"
+make clean build_libs AR="llvm-ar" RANLIB="llvm-ranlib" CC=x86_64-linux-android21-clang CFLAG="-fPIC -DOPENSSL_THREADS -D_REENTRANT -DDSO_DLFCN -DHAVE_DLFCN_H -D__ANDROID_API__=21 -Os -fomit-frame-pointer -Wall -I$ANDROID_NDK_ROOT/sysroot/usr/include -I$ANDROID_NDK_ROOT/sysroot/usr/include/i686-linux-android"
 #mkdir -p x86/lib x86/include/openssl
 #cp lib*.a x86/lib
 #cp include/openssl/* x86/include/openssl
